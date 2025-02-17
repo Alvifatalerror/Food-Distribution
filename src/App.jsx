28b7807fde449fff6a5845ac21da4139f4cafa4a
Shaun MR <shaunmr009@gmail.com>
2025-02-17 13:19:16 +0530

    contact portion added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
 import Footer from './components/Footer'
+
+
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
@@ -11,6 +14,7 @@ const App = () => {
       <About />
       <Projects />
       <Testimonials/>
+      <Contact />
       <Footer/>
     </div>
   )
