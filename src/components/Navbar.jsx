f480872a9307d2c8bb7f73922c5083a7ab134a8c
Riyabenny05 <riyabenny2005@gmail.com>
2025-02-22 12:13:52 +0530

    Projects page updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
 useEffect(()=>{
   if(showMobileMenu){
-    document.body.style.overflow = 'hidden'
+    document.body.style.overflow = 'hidden';
   }else{
     document.body.style.overflow = 'auto';
   }
