ac785cfcc0b2ba92d9e976feaad6161054327bc2
Shaun MR <shaunmr009@gmail.com>
2025-02-27 12:11:53 +0530

    Dashboard corrected

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,20 +18,7 @@ const Sidebar = ({
         onClick={toggleSidebar}
         className="md:hidden absolute top-5 right-2 z-20 p-1 bg-gray-100 rounded-xl"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-4 w-4"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z"></path></svg>
       </button>
 
       <h2 className="text-2xl font-bold mb-6 text-green-500 text-left">
@@ -56,14 +43,7 @@ const Sidebar = ({
                 Add Waste
               </button>
             </li>
-            <li className="mb-2">
-              <button
-                onClick={() => onViewChange("collectWaste")}
-                className="w-full text-left p-2 rounded transition duration-300 font-medium text-gray-700 hover:bg-gray-100 hover:text-green-500"
-              >
-                Collect Waste
-              </button>
-            </li>
+            
           </>
         ) : (
           <>
